1bd4d946974b3d0d64f7b34a8ca7c1b9b327452f
maggiegalletti <maggiegalletti@gmail.com>
2025-04-10 15:22:04 -0700

    some code from template push

---
+++ login-page.js
@@ -0,0 +1,16 @@
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const password = loginForm.password.value;
+
+    if (password === "nousmasseter") {
+        alert("You have successfully entered the password.");
+        window.location.href = "https://ka-denz.github.io/forums/";;
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
